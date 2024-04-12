6c78220718f60a63f2a5db81100260bde0c48de6
qiaohaiqiang <11qiaoqi@163.com>
2024-04-12 22:53:13 +0800

    73

--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -12,7 +12,7 @@
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::borrow::Cow;
 
@@ -50,6 +50,9 @@ mod tests {
         match abs_all(&mut input) {
             // TODO
             Cow::Borrowed(_) => Ok(()),
+            _ => Err("Expected borrowed value"),
+
+            
         }
     }
 
@@ -62,6 +65,8 @@ mod tests {
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
             // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
     }
 
@@ -74,6 +79,8 @@ mod tests {
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
             // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
 
 
         }
