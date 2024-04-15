c0546c293390b2856a834d35819f15e692df29f9
qiaohaiqiang <11qiaoqi@163.com>
2024-04-15 23:07:35 +0800

    100

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,13 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        if s.split(',').count() != 2 {
+            return Err(ParsePersonError::BadLen);
+        };
+       
         let (name, age) = match s.split_once(',') {
             Some(x) => x,
             None => return Err(ParsePersonError::NoName),
